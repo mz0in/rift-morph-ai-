04c4e284b2e222e65d8df98eb9641fc2184d9578
Ed Ayers <contact@edayers.com>
2023-06-26 23:28:22 +0100

    doc: add a boat-load of docs to jsonrpc.py

--- rift-engine/rift/rpc/jsonrpc.py
+++ rift-engine/rift/rpc/jsonrpc.py
@@ -30,6 +30,13 @@ logger = logging.getLogger(__name__)
 
 
 class ErrorCode(Enum):
+    """Error codes for JSON-RPC.
+
+    Reference:
+    - https://www.jsonrpc.org/specification#error_object
+    - https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#errorCodes
+    """
+
     ### JSON-RPC codes
 
     parse_error = -32700
@@ -74,15 +81,25 @@ encoder = MyJsonEncoder()
 
 @dataclass
 class Request:
+    """A request object for JSON-RPC.
+
+    reference: https://www.jsonrpc.org/specification#request_object
+    """
+
     method: str
+    """A string identifying the RPC method to call."""
     id: Optional[Union[str, int]] = field(default=None)
+    """Unique identifier of the request, the response object should use the same request id."""
     params: Optional[Any] = field(default=None)
+    """A structured value that holds the parameter values to be used during the invocation of the method."""
 
     @property
     def is_notification(self):
+        """True if this is a notification request. Notifications do not have an id and so can't be responded to."""
         return self.id is None
 
     def to_bytes(self):
+        """Encode the request as bytes. Note that this will automatically convert Python objects to JSON using MyJsonEncoder."""
         return encoder.encode(self).encode()
 
     def __str__(self):
@@ -94,6 +111,13 @@ class Request:
 
 @dataclass
 class ResponseError(Exception):
+    """A response error object in JSON-RPC.
+
+    Note that you can raise these as you would an exception.
+    If you raise this in an RPC method handler, the error will be sent to the client.
+
+    ref: https://www.jsonrpc.org/specification#error_object"""
+
     code: ErrorCode
     message: str
     data: Optional[Any] = field(default=None)
@@ -103,10 +127,16 @@ class ResponseError(Exception):
 
 
 def invalid_request(message: str) -> ResponseError:
+    """Raise this to tell JSON-RPC that the request was invalid.
+
+    You should use this if the request does not parse as JSON or is not a valid Request or Response.
+    If it is a valid JSON-RPC object but invalid parameters then use `invalid_params` instead.
+    """
     return ResponseError(ErrorCode.invalid_request, message)
 
 
 def method_not_found(method_name: str) -> ResponseError:
+    """Raise this if the given method name (Request.method) is not found."""
     return ResponseError(
         ErrorCode.method_not_found,
         f"no method found for {method_name}",
@@ -115,10 +145,15 @@ def method_not_found(method_name: str) -> ResponseError:
 
 
 def invalid_params(message: str = "invalid params") -> ResponseError:
+    """Raise this if the parameters of the request are invalid."""
     return ResponseError(ErrorCode.invalid_params, message)
 
 
 def internal_error(message: str) -> ResponseError:
+    """This should be raised if the server code has an internal error that is not the fault of the client.
+
+    Any exceptions raised in a method handler will be caught and converted to this error.
+    """
     return ResponseError(ErrorCode.internal_error, message)
 
 
@@ -149,7 +184,14 @@ class Response:
 
 
 class Dispatcher:
-    """Dispatcher for JSON-RPC requests."""
+    """Dispatcher for JSON-RPC requests.
+
+    It is a dictionary mapping method names to python functions to handle these methods.
+    The Python function should have a single argument.
+    If the python function's argument and return type are annotated, then the dispatcher will use
+    `todict` and `fromdict` to convert the arguments to and from JSON.
+
+    """
 
     def __init__(self, methods=None, extra_kwargs={}):
         self.methods = methods or {}
@@ -206,6 +248,7 @@ class Dispatcher:
 
 
 server_count = 0
+"""Counter for labelling servers with a unique id."""
 
 RequestId = Union[str, int]
 
@@ -221,8 +264,11 @@ class InitializationMode(Enum):
 
 class RpcServerStatus(Enum):
     preinit = 0
+    """Initialize has not been called yet."""
     running = 1
+    """The server is running."""
     shutdown = 2
+    """The server has been shutdown."""
 
 
 class ExitNotification(Exception):
@@ -230,7 +276,21 @@ class ExitNotification(Exception):
 
 
 def rpc_method(name: Optional[str] = None):
-    """Decorate your method with this to say that you are implementing a JSON-RPC method."""
+    """Decorate your method with this to say that you are implementing a JSON-RPC method.
+
+    Example:
+    ```
+    class MyServer(RpcServer):
+        @rpc_method('foo')
+        def foo(self, params : int) -> str:
+            return f"foo {params}"
+    ```
+
+    Methods should have a single argument.
+    Methods can be async.
+    If the method's argument is annotated with type T, then the argument will be converted from JSON to T using `fromdict(T, ·)`.
+    Most builtin types are supported, as well as dataclasses and pydantic models.
+    """
 
     def decorator(fn):
         setattr(fn, "rpc_method", name or fn.__name__)
@@ -240,7 +300,25 @@ def rpc_method(name: Optional[str] = None):
 
 
 def rpc_request(name: Optional[str] = None):
-    """Decorate your _stub_ method with this to have a client RPC."""
+    """Decorate your _stub_ method with this to have a client RPC.
+
+    The resulting class method will call the server's method with the given name.
+    If a return annotation is given, the JSON result will be converted to that type using `fromdict(T, ·)`.
+
+    # Example:
+    ```
+    class MyClient(RpcServer):
+        @rpc_request('foo')
+        def foo(self, params : int) -> str:
+            # this is never called, it is just a stub
+            ...
+
+
+    client = MyClient()
+    x = await client.foo(42)
+    assert x == "foo 42"
+    ```
+    """
 
     def decorator(fn):
         assert asyncio.iscoroutinefunction(fn)
@@ -257,24 +335,51 @@ def rpc_request(name: Optional[str] = None):
 class RpcServer:
     """Implementation of a JSON-RPC server.
 
+    To create your own server, subclass this class and implement the methods you want to support.
+
+    ```
+    class MyServer(RpcServer):
+
+        @rpc_method('foo')
+        def foo(self, params : int) -> str:
+            return f"foo {params}"
+
+    transport = ...
+    server = MyServer(transport)
+
+    asyncio.run(server.serve_forever())
+
+    ```
+
+    # Extra functionality
+
     Following the conventions of LSP for extra functionality.
 
-    Builtin methods:
-    - "$/cancelRequest" notification will cancel the given request id.
-      https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#cancelRequest
-    - "shutdown" will put the server into a shutdown state.
-      The 'shutdown' handler on the dispatcher will be called.
-      All in-flight requests will be cancelled.
-      No further requests or notifications will be sent.
-      https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#shutdown
-    - "exit" will cause the server to exit immediately (without replying to requests)
+    ## Cancellation
 
-    [todo] special case for initialized; don't let other methods be called until initialized method is called.
-    [todo] implement progress support
-    [todo] rename to RpcConnection, then RpcServer and RpcClient handle the different conventions for
+    The server will listen for the "$/cancelRequest" notification.
+    Upon recieving this, the in-flight `@rpc_method` task for the request will be cancelled.
+    More details can be found [here](https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#cancelRequest).
+
+    ## Initialisation
+
+    When you create a server, you can specify an `initialization_mode` as an argument to `__init__()`.
+    If this is set to `ExpectInit`, then the server will wait for an `initialize` request from the client before accepting any other requests.
+    You can write custom code to be performed on initialization by making an `@rpc_method('initialize')` method handler (LSP _servers_ are `ExpectInit`).
+    If this is set to `SendInit`, then the server will send an `initialize` request to the client before accepting any other requests (LSP _clients_ (ie editors) are `SendInit`).
+    A `NoInit` server will accept requests immediately.
+
+    ## Shutdown
+
+    A "shutdown" request will put the server into a shutdown state.
+    All in-flight requests will be cancelled and no more notifications or requests will be accepted or sent.
+    You can write custom code to be performed on shutdown by making an `@rpc_method('shutdown')` method handler.
+    Once in the shutdown state, the server will stay alive until the client sends an `exit` notification.
+    An `exit` notification will kill the server immediately.
+
+    [todo] rename to RpcConnection, then RpcServer and RpcClient handle the different Init conventions for
     lifecycle.
     [todo] add warnings if requests go unanswered for too long.
-    [todo] instead of needing to explicitly register methods, make a decorator.
     """
 
     dispatcher: Dispatcher
@@ -282,8 +387,9 @@ class RpcServer:
     transport: Transport
     init_mode: InitializationMode
     name: str
+    """ This is a human-readable name that will be used in log messages."""
     request_counter: int
-    """ Unique id for each request I make to the peer. """
+    """ Unique id for each request I make to my peer. """
     my_requests: dict[RequestId, Future[Any]]
     """ Requests that I have made to my peer. """
     their_requests: dict[RequestId, Task]
@@ -318,7 +424,7 @@ class RpcServer:
         self.dispatcher = dispatcher or Dispatcher()
         self.my_requests = {}
         self.their_requests = {}
-        self.request_counter = 1000
+        self.request_counter = 1000 * server_count
         self.notification_tasks = set()
 
         for name, method in inspect.getmembers(self, predicate=inspect.ismethod):
@@ -333,18 +439,36 @@ class RpcServer:
     def __str__(self):
         return self.name
 
-    async def send(self, r: Union[Response, Request]):
+    async def _send(self, r: Union[Response, Request]):
         await self.transport.send(r.to_bytes())
 
     async def notify(self, method: str, params: Optional[Any]):
+        """Send a notification to the peer."""
         if self.status != RpcServerStatus.running:
             raise RuntimeError(
                 f"can't send notifications while server is in {self.status.name} state"
             )
         req = Request(method=method, params=params)
-        await self.send(req)
+        await self._send(req)
 
     async def request(self, method: str, params: Optional[Any]) -> Any:
+        """Send a request to the peer and wait for a response.
+
+        Args:
+            - method: the name of the method to call.
+            - params: the parameters to pass to the method, it can be any python object that will be converted to JSON using `MyJsonEncoder`.
+
+        Returns:
+            An awaitable that yields a json-like python object (ie something that you would get from `json.loads()`) representing the response from the peer.
+
+        Raises:
+            - RuntimeError: if the server is not in the running state.
+            - ResponseError: if the peer responds with an error, you can use ResponseError.code to determine the cause of the error.
+
+        Todo:
+           - Cancellation support: rather than a coro it should return a cancellable future.
+           - timeout?
+        """
         if self.status != RpcServerStatus.running:
             if self.init_mode != InitializationMode.SendInit or method != "initialize":
                 raise RuntimeError(
@@ -360,13 +484,15 @@ class RpcServer:
         if id in self.my_requests:
             raise RuntimeError(f"non-unique request id {id} found")
         self.my_requests[id] = fut
-        await self.send(req)
+        await self._send(req)
         result = await fut
         return result
 
     async def _send_init(self, init_param):
+        """Send an initialization request to the peer."""
         await self.request("initialize", init_param)
         self.status = RpcServerStatus.running
+        # [todo] allow inheriting classes to do things here.
         await self.notify("initialized", None)
 
     async def serve_forever(self, init_param=None):
@@ -375,6 +501,11 @@ class RpcServer:
         It will return when:
         - the transport closes gracefully
         - the exit notification is received.
+        - a keyboard interrupt is received (ie, the user presses ctrl-c)
+
+        Args:
+            - init_param is a parameter that will be passed to the peer's `initialize` method,
+              assuming that this server is in `InitializationMode.SendInit` mode.
 
         Raises:
             - TransportClosedError:the transport closes with an error
@@ -399,8 +530,8 @@ class RpcServer:
                 try:
                     data = await self.transport.recv()
                     messages = json.loads(data)
-                    # res can be a batch
                     if isinstance(messages, dict):
+                        # datagram contains a single message
                         messages = [messages]
                     elif not isinstance(messages, list):
                         raise TypeError(f"expected list or dict, got {type(messages)}")
@@ -414,7 +545,7 @@ class RpcServer:
                     response = Response(
                         error=ResponseError(message=str(e), code=ErrorCode.parse_error)
                     )
-                    await self.send(response)
+                    await self._send(response)
                     continue
                 except ExitNotification as e:
                     logger.info(f"{self.name} received exit notification")
@@ -429,7 +560,10 @@ class RpcServer:
                     logger.error(f"{self.name} transport error:\n{e}")
                     raise e
                 except Exception as e:
-                    logger.exception(f"{self.name} unhandled {type(e).__name__}:\n{e}")
+                    # we should only reach here if there is a bug in the RPC code.
+                    logger.exception(
+                        f"{self.name} unhandled {type(e).__name__}:\n{e}\nThis likely caused by a bug in the RPC server code."
+                    )
                     raise e
         finally:
             logger.info(f"exiting serve_forever loop")
@@ -441,15 +575,17 @@ class RpcServer:
             self._shutdown()
 
     def _shutdown(self):
+        # [todo] also send cancel notifications to all our pending request futures.
         for t in self.their_requests.values():
             t.cancel("shutdown")
         for t in self.notification_tasks:
             t.cancel("shutdown")
         self.status = RpcServerStatus.shutdown
+        logger.info(f"{self} entered shutdown state")
 
     def _handle_message(self, message: Any):
         if "result" in message or "error" in message:
-            # this is a Response
+            # message is a Response
             res = ofdict(Response, message)
             if res.id not in self.my_requests:
                 logger.error(f"received response for unknown request: {res}")
@@ -465,9 +601,10 @@ class RpcServer:
                 else:
                     fut.set_result(res.result)
         else:
-            # this is a request.
+            # message is a Request.
             req = ofdict(Request, message)
             if req.method == "exit":
+                # exit notification should kill the server immediately.
                 if self.status != RpcServerStatus.shutdown:
                     logger.warning("exit notification received before shutdown request")
                 # https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#exit
@@ -480,20 +617,23 @@ class RpcServer:
             )
             id = req.id
             if id is not None:
+                # Request expects a reponse
                 if id in self.their_requests:
                     raise invalid_request(f"request id {id} is already in use")
                 self.their_requests[id] = task
                 task.add_done_callback(lambda _: self.their_requests.pop(id))
             else:
+                # Request is a notification, no response expected.
                 self.notification_tasks.add(task)
                 task.add_done_callback(self.notification_tasks.discard)
 
     async def _on_request(self, req: Request) -> None:
+        """Handles a request from the peer."""
         try:
             result = await self._on_request_core(req)
         except asyncio.CancelledError as e:
             if not req.is_notification:
-                await self.send(
+                await self._send(
                     Response(
                         id=req.id,
                         error=ResponseError(
@@ -502,10 +642,13 @@ class RpcServer:
                     )
                 )
         except ResponseError as e:
-            await self.send(Response(id=req.id, error=e))
+            await self._send(Response(id=req.id, error=e))
         except Exception as e:
-            logger.exception(f"{self} {req} unhandled {type(e).__name__}:\n{e}")
-            await self.send(
+            # if we get here, the method handler code has a bug.
+            logger.exception(
+                f"{self} {req} unhandled {type(e).__name__}:\n{e}\nThis is likely caused by a bug in the {req.method} method handler."
+            )
+            await self._send(
                 Response(
                     id=req.id,
                     error=ResponseError(code=ErrorCode.server_error, message=str(e)),
@@ -513,11 +656,15 @@ class RpcServer:
             )
         else:
             if not req.is_notification:
-                await self.send(Response(id=req.id, result=result))
+                await self._send(Response(id=req.id, result=result))
             else:
-                assert result is None, "notification handlers must return None"
+                if result is not None:
+                    logger.warning(
+                        f"notification handler {req.method} returned a value, this will be ignored"
+                    )
 
     async def _on_request_core(self, req: Request):
+        """Inner part of self._on_request, without error handling."""
         if self.status == RpcServerStatus.preinit:
             INIT_METHOD = "initialize"
             if self.init_mode == InitializationMode.ExpectInit:
@@ -546,9 +693,11 @@ class RpcServer:
                 raise invalid_request("cancel request must be a notification")
             if not isinstance(req.params, dict) or not "id" in req.params:
                 raise invalid_params('params must be a dict with "id" key')
-            t = self.their_requests.get(req.params["id"], None)
+            id = req.params["id"]
+            t = self.their_requests.get(id, None)
             if t is not None:
                 t.cancel("requested by peer")
+            # if t is None then the request has already completed and removed itself from self.their_requests
             return None
 
         if req.method not in self.dispatcher:
